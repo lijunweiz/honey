7e5e2166c30563d885a3928361957d707b4fce49
lijunweiz <takealeaf@yeah.net>
2024-11-17 18:54:16 +0800

    系统简介

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,7 +2,30 @@
   <div class="dashboard-container">
     <!--    <div class="dashboard-text">name: {{ name }}</div>-->
     <!--    <div class="dashboard-text">roles: <span v-for="role in roles" :key="role">{{ role }}</span></div>-->
-    你好
+    <h2 class="dashboard-text" style="color: red">金融风控决策引擎-智能风控</h2>
+    <div v-show="false">beaver</div>
+    <div class="dashboard-text">
+      <div>
+        <p>目标</p>
+        <ul>
+          <li>数据源配置</li>
+          <li>变量配置</li>
+          <li>表达式</li>
+          <li>评分卡</li>
+          <li>流程编排</li>
+          <li>权限控制</li>
+        </ul>
+      </div>
+      <div>
+        <p>特点</p>
+        <ul>
+          <li>灵活性：及时响应业务变化，生产实时变更，无需系统重启</li>
+          <li>易用性：业务人员只需稍加学习即可入门本系统的使用，学习成本低</li>
+          <li>易维护性：技术人员掌握基本技术，无需过多研究即可入门</li>
+          <li>扩展性：系统支持高度扩展，仅需修改较少代码即可实现功能的插拔</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
